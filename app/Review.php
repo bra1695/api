e07d9d4c1757208b638cd5f7fd204d9ef682e009
brah17 <brahimhm470@gmail.com>
2018-07-06 18:16:11 +0200

    relationship

--- app/Review.php
+++ app/Review.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Product;
 class Review extends Model
 {
-    //
+    public function product(){
+    	$this->belongTo(Product::class);
+    }
 }
